6eae7cd143e63c6b44e1604a3d1f79ede8eb8875
theshiv <apshively@gmail.com>
2019-06-17 09:38:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/java-8-debian.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
-apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git
+apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git software-properties-common
 add-apt-repository -y ppa:webupd8team/java
 apt-get update && apt-get -y install oracle-java8-installer
 apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git apt-transport-https oracle-java8-installer
