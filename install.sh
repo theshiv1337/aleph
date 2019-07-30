5c2599254aaf1d0eec558ebf37933c10e0177060
theshiv <apshively@gmail.com>
2019-07-29 20:45:06 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ echo ""
 echo " [ Installing a bunch of shit ] "
 echo ""
 apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git apt-transport-https software-properties-common default-jdk python-pip
-git clone https://github.com/chrishantha/install-java.git
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update && apt-get -y install elasticsearch
