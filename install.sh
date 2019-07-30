ddfd12d782fc0a263110931f6b9ebc70a436506c
theshiv <apshively@gmail.com>
2019-07-29 20:32:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
  
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/java-8-debian.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/java-8-debian.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
-apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git software-properties-common
+apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git apt-transport-https software-properties-common default-jre python-pip
 git clone https://github.com/chrishantha/install-java.git
-apt-get install openjdk-8-jdk-headless -y --force-yes
-add-apt-repository -y ppa:webupd8team/java
-apt-get update && apt-get -y install oracle-java8-installer
-apt-get install -y python-pyrex libffi-dev libfuzzy-dev python-dateutil libsqlite3-dev python-pip git apt-transport-https oracle-java8-installer
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update && apt-get -y install elasticsearch
-cd /opt
+cp /opt/aleph/settings.py.orig /opt/aleph/settings.py
