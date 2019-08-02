ded8485203418760ef204f7020324e278be94d96
theshiv <apshively@gmail.com>
2019-08-02 12:29:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo ""
 echo " [ Installing ElasticSearch ] "
 echo ""
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
 apt-get update && apt-get -q -y install elasticsearch
 cp /opt/aleph/aleph/settings.py.orig /opt/aleph/aleph/settings.py
 echo ""
